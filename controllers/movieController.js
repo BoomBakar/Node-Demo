34e7777f55a8965298507ab1022d72702991beae
= <=>
2023-08-16 20:21:17 +0500

    controller added

---
+++ controllers/movieController.js
@@ -0,0 +1,42 @@
+const movies = require('../models/movies');
+const validateMovie = require('../validator/validateMovie');
+
+const movieController = {
+    getAllMovies: (req, res) => {
+        res.send(movies);
+    },
+    getMovieById: (req, res) => {
+        const movie = movies.find(m => m.id === parseInt(req.params.id));
+        if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+        res.status(200).send(movie);
+    },
+    addMovie: (req, res) => {
+        const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const movie = {
+        id: movies.length + 1,
+        title: req.body.title,
+        year: req.body.year
+    };
+    movies.push(movie);
+    res.status(200).send(movie);
+    },
+    updateMovie: (req, res) => {
+        const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    movie.title = req.body.title;
+    movie.year = req.body.year;
+    res.status(200).send(movie);
+    },
+    deleteMovie: (req, res) => {
+        const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    const index = movies.indexOf(movie);
+    movies.splice(index, 1);
+    res.status(200).send(movie);
+    }
+};
+
+module.exports = movieController;
--- router/routes.js
+++ router/routes.js
@@ -1,54 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const validateMovie = require('../validator/validateMovie');
-const movies = require('../models/movies');
+const movieController = require('../controllers/movieController');
 
 
-router.get('/', (req, res) => {
-    console.log(req);
-    res.send('Working!');
-});
 //Get request for all movies
-router.get('/movies', (req, res) => {
-    //console.log(res);
-    res.send(movies);
-});
+router.get('/movies', movieController.getAllMovies);
 //Get request for a specific movie
-router.get('/movies/:id', (req, res) => {
-    const movie = movies.find(m => m.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-    res.status(200).send(movie);
-});
+router.get('/movies/:id', movieController.getMovieById);
 //Post request to add a movie
-router.post('/movies', (req, res) => {
-    const { error } = validateMovie(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    const movie = {
-        id: movies.length + 1,
-        title: req.body.title,
-        year: req.body.year
-    };
-    movies.push(movie);
-    res.status(200).send(movie);
-});
+router.post('/movies', movieController.addMovie);
 //Put request to update a movie
-router.put('/movies/:id', (req, res) => {
-    const movie = movies.find(m => m.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-    const { error } = validateMovie(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    movie.title = req.body.title;
-    movie.year = req.body.year;
-    res.status(200).send(movie);
-});
+router.put('/movies/:id', movieController.updateMovie);
 //Delete request to delete a movie
-router.delete('/movies/:id', (req, res) => {
-    const movie = movies.find(m => m.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-    const index = movies.indexOf(movie);
-    movies.splice(index, 1);
-    res.status(200).send(movie);
-});
+router.delete('/movies/:id', movieController.deleteMovie);
 
 
 module.exports = router;
