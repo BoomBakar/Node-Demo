fb21ffe6a637ae3e1834ba9150686490d2178fbc
= <=>
2023-08-17 13:05:50 +0500

    ..

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const routes = require('./router/routes');
+const movieRouter = require('./router/routes');
 const auth = require('./middlewares/auth');
 
 app.use(express.json());
 
 app.use(auth);
 
-app.use('/api', routes);
+app.use('/api', movieRouter);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on port http://localhost:${process.env.PORT || 3000}`);
+    console.log(`Server started on port http://localhost:${process.env.PORT || 3000}/api/movies`);
 });
 
 
